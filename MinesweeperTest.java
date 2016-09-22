86e10f85a8d04912afb9cccd5849d9cf1aa79d19
bamtonio <bamtonio@gmail.com>
2016-09-22 00:51:34 -0500

    Update MinesweeperTest.java
    edited last test so it also tests that all NON-mine cells are exposed.

--- MinesweeperTest.java
+++ MinesweeperTest.java
@@ -422,11 +422,16 @@ public class MinesweeperTest {
     public void gameWonWhenAllMinesAreSealedAndOtherCellsAreExposed()
     {
         Minesweeper minesweeper = new Minesweeper(noMines);
-
-        for(int i = 0; i < 10; i++)
+        for(int x = 0; x < 10; x++)
         {
-            minesweeper.mines[i][0] = true;
-            minesweeper.toggleSeal(i, 0);
+            minesweeper.mines[x][0] = true;
+            minesweeper.toggleSeal(x, 0);
+
+        }
+        for(int i = 0; i< 10; i++) {
+            for(int j = 1; j < 10; j++) {
+                minesweeper.exposeCell(i,j);
+            }
         }
 
         assertEquals(GameStatus.WON, minesweeper.getGameStatus());
